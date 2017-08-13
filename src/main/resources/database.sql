bc399c7f1b30fc42904bc27ff52deb05c1d1b904
Alexander <berezkin05@gmail.com>
2017-08-13 19:43:57 +0300

    populated orders and menus

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -14,3 +14,27 @@ CREATE TABLE menu_dish (
 );
 
 ALTER TABLE orders ADD COLUMN status VARCHAR(255);
+
+INSERT INTO orders VALUES (1, 1, 1, 'Sun Aug 13 19:07:40 EEST 2017', 'OPEN');
+INSERT INTO orders VALUES (2, 1, 2, 'Sun Aug 12 19:08:40 EEST 2017', 'CLOSE');
+INSERT INTO orders VALUES (3, 2, 3, 'Sun Aug 13 19:09:40 EEST 2017', 'OPEN');
+INSERT INTO orders VALUES (4, 2, 4, 'Sun Aug 12 19:10:40 EEST 2017', 'CLOSE');
+
+INSERT INTO dish_to_order VALUES (1, 1);
+INSERT INTO dish_to_order VALUES (1, 2);
+INSERT INTO dish_to_order VALUES (2, 3);
+INSERT INTO dish_to_order VALUES (2, 4);
+INSERT INTO dish_to_order VALUES (3, 1);
+INSERT INTO dish_to_order VALUES (3, 3);
+INSERT INTO dish_to_order VALUES (4, 2);
+INSERT INTO dish_to_order VALUES (4, 4);
+
+INSERT INTO menu VALUES (1, 'breakfast');
+INSERT INTO menu VALUES (2, 'lunch');
+
+INSERT INTO menu_dish VALUES (1, 1);
+INSERT INTO menu_dish VALUES (1, 2);
+INSERT INTO menu_dish VALUES (1, 3);
+INSERT INTO menu_dish VALUES (2, 2);
+INSERT INTO menu_dish VALUES (2, 3);
+INSERT INTO menu_dish VALUES (2, 4);
